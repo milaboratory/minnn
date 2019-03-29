cd2e154252c2f817ac1cb2fe478ef52b0e9318e3
Alexander230 <fire_2005@mail.ru>
2019-03-29 13:32:14 +0300

    Tests for pipes (both failed; bugs found).

--- itests/case_pipes.sh
+++ itests/case_pipes.sh
@@ -2,4 +2,13 @@
 
 set -euxo pipefail
 
-true
+prefix=pipes_t1
+input="test01_R1.fastq.gz test01_R2.fastq.gz"
+minnn extract -n 100000 --input ${input} --pattern "^(UMI:NNNNNNNN)\*" |
+    minnn sort -f --groups UMI --output ${prefix}_sorted.mif
+
+prefix=pipes_t2
+minnn extract -n 100000 --input ${input} --pattern "^(UMI:NNNNNNNN)\*" |
+    minnn sort --groups UMI --output ${prefix}_sorted.mif |
+    minnn consensus --groups UMI |
+    minnn mif2fastq -f --group R1=${prefix}_R1.fastq R2=${prefix}_R2.fastq
