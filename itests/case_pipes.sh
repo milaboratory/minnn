059b80b58e9fa5fced9072944467e8f124967828
Alexander230 <fire_2005@mail.ru>
2019-04-02 14:54:38 +0300

    Test for smart overwrite.

--- itests/case_pipes.sh
+++ itests/case_pipes.sh
@@ -2,13 +2,12 @@
 
 set -euxo pipefail
 
-prefix=pipes_t1
+prefix=pipes_test1
 input="test01_R1.fastq.gz test01_R2.fastq.gz"
 minnn extract -n 100000 --input ${input} --pattern "^(UMI:NNNNNNNN)\*" |
     minnn sort -f --groups UMI --output ${prefix}_sorted.mif
 
-prefix=pipes_t2
+prefix=pipes_test2
 minnn extract -n 100000 --input ${input} --pattern "^(UMI:NNNNNNNN)\*" |
-    minnn sort --groups UMI --output ${prefix}_sorted.mif |
-    minnn consensus --groups UMI |
+    minnn sort --groups UMI | minnn consensus --groups UMI |
     minnn mif2fastq -f --group R1=${prefix}_R1.fastq R2=${prefix}_R2.fastq
