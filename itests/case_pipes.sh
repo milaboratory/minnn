9c28de3b9d7a4289b54a0078c70b809bbb742285
Alexander230 <fire_2005@mail.ru>
2020-05-06 16:55:31 +0300

    Fix for integration test case_pipes.

--- itests/case_pipes.sh
+++ itests/case_pipes.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 
+trap "exit 1" TERM
+export SCRIPT_PID=$$
+
 set -euxo pipefail
 
-input="test01_R1.fastq.gz"
+assert_return_141() {
+    if [[ $1 -ne 141 ]]; then
+        >&2 echo Return value 141 expected, got $1
+        kill -s TERM $SCRIPT_PID
+    fi
+}
 
 prefix=fastq_pipe_extract_test
-zcat ${input} | minnn extract -n 100000 --pattern "^(UMI:NNNNNNNN)" --output ${prefix}_extracted.mif
+input="test01_R1.fastq.gz"
+set +eo pipefail
+(zcat ${input} ; assert_return_141 $?) | minnn extract -n 100000 --pattern "^(UMI:NNNNNNNN)" --output ${prefix}_extracted.mif
+set -eo pipefail
+zcat ${input} | minnn extract -f -n 1000000 --pattern "^(UMI:NNNNNNNN)" --output ${prefix}_extracted.mif
+zcat ${input} | minnn extract -f --pattern "^(UMI:NNNNNNNN)" --output ${prefix}_extracted.mif
